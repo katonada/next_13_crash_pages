3e4134dde203a1fc4f5c80aacfd1f9d1b7e53d06
katonada <davor.katona.sesto@wm.rs>
2023-08-07 16:55:30 +0200

    add SSR functionality to app

--- src/pages/coins/index.jsx
+++ src/pages/coins/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import Image from 'next/image';
+import axios from 'axios';
 
-const coinList = () => {
+const coinList = ({coinData}) => {
+
+    console.log({coinData});
+
     return (
         <div>
-
+            <h1>Coin List</h1>
+            {
+                coinData.coins.map(coin => {
+                    return (
+                        <div key={coin.id}>
+                            <h2>{coin.name}</h2>
+                            <Image src={coin.icon} alt={coin.name} width={50} height={50}/>
+                            <p>{coin.price}</p>
+                        </div>
+                    );
+                })
+            }
         </div>
     );
+};
+
+// export const getStaticProps = async ({params}) => {
+export const getServerSideProps = async ({params}) => {
+    const id = params?.id;
+    const data = await axios.get('https://api.coinstats.app/public/v1/coins?skip=0');
+
+    return {
+        props: {
+            coinData: data.data
+        }
+    };
 };
 
 export default coinList;
